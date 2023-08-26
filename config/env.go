b9fdb74990b5e2ed8b597988a071b9682c6e0633
bobikenobi12 <borislav1207@gmail.com>
2023-08-26 22:27:10 +0300

    add env config for db connection

---
+++ config/env.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+	"os"
+)
+
+func LoadENV() error {
+	goEnv := os.Getenv("GO_ENV")
+
+	if goEnv == "" || goEnv == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
